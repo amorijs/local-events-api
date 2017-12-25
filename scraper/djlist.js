1eb2be0e13ee4d84a46a59feb5437def72f9881d
chris <chris.amori93@gmail.com>
2017-12-25 14:48:03 -0800

    Refactor djlist scraper. Implement Async/Await + improve error handling

--- scraper/djlist.js
+++ scraper/djlist.js
@@ -1,56 +1,49 @@
 'use strict';
-const request = require('request');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-const getEvents = url => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, html) => {
-      if (error) reject(new Error(`Could not get events: ${err.message}`));
+const getEvents = async function(url) {
+  try {
+    const html = await rp(url);
+    const $ = cheerio.load(html);
+    const eventPromises = [];
 
-      const $ = cheerio.load(html);
-      const eventPromises = [];
+    $('.rows')
+      .children('li')
+      .each((i, eventItem) => eventPromises.push(parseEvent(eventItem)));
 
-      $('.rows')
-        .children('li')
-        .each((i, eventItem) => eventPromises.push(parseEvent(eventItem)));
-
-      Promise.all(eventPromises)
-        .then(data => resolve(data))
-        .catch(e => console.log(e));
-    });
-  });
+    return Promise.all(eventPromises);
+  } catch (err) {
+    throw new Error(`Could not get event at url: ${url}. Error message: ${err.message}`);
+  }
 };
 
-const parseEvent = eventElement => {
-  return new Promise((resolve, reject) => {
-    const $ = cheerio.load(eventElement);
+const parseEvent = function(eventElement) {
+  const $ = cheerio.load(eventElement);
 
-    const name = $('.event-name')
-      .find('h2')
-      .text()
-      .trim();
+  const name = $('.event-name')
+    .find('h2')
+    .text()
+    .trim();
 
-    const location = $('.event-name')
-      .find('h3')
-      .text()
-      .trim();
+  const location = $('.event-name')
+    .find('h3')
+    .text()
+    .trim();
 
-    const date = parseDate($('.event-date')).toDateString();
+  const date = parseDate($('.event-date')).toDateString();
 
-    const onclickUrl = $('.event-buttons')
-      .find('button')
-      .attr('onclick');
+  const onclickUrl = $('.event-buttons')
+    .find('button')
+    .attr('onclick');
 
-    const parsedUrl = parseUrl(onclickUrl);
-    const furtherDetailsPromise = getEventPageData(parsedUrl);
+  const parsedUrl = parseUrl(onclickUrl);
+  const furtherDetailsPromise = getEventPageData(parsedUrl);
 
-    furtherDetailsPromise
-      .then(details => resolve({ name, location, date, details }))
-      .catch(e => reject(e));
-  });
+  return furtherDetailsPromise.then(details => ({ name, location, date, details }));
 };
 
-const parseDate = dateElement => {
+const parseDate = function(dateElement) {
   const $ = cheerio.load(dateElement);
   let dateString = '';
 
@@ -61,17 +54,17 @@ const parseDate = dateElement => {
   return new Date(dateString + '2017');
 };
 
-const getEventPageData = url => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, html) => {
-      if (error) reject(new Error(`Could not scrape event page: ${err.message}`));
-      const $ = cheerio.load(html);
-      resolve($('.info-extra').text());
-    });
-  });
+const getEventPageData = async function(url) {
+  try {
+    const html = await rp(url);
+    const $ = cheerio.load(html);
+    return $('.info-extra').text();
+  } catch (err) {
+    throw new Error(`Could not scrape event page: ${err.message}`);
+  }
 };
 
-const parseUrl = onClickString => {
+const parseUrl = function(onClickString) {
   const startIndex = onClickString.indexOf("'");
   const endIndex = onClickString.indexOf("'", startIndex + 1);
   return onClickString.slice(startIndex + 1, endIndex);
@@ -79,17 +72,23 @@ const parseUrl = onClickString => {
 
 const sortByDate = data => data.sort((a, b) => new Date(a.date) - new Date(b.date));
 
-const updateCache = () =>
-  getEvents('http://thedjlist.com/events/los-angeles-us/')
+const fetchData = function() {
+  return getEvents('http://thedjlist.com/events/los-angeles-us/')
     .then(data => sortByDate(data))
-    .then(sortedData => (cache = sortedData))
-    .then(() => console.log('Successfully updated cache!'))
     .catch(e => console.log(e));
+};
+
+const updateCache = cache => {
+  console.log('Updating djlist cache...');
+  fetchData()
+    .then(data => (cache = data))
+    .then(() => console.log('Successfully updated djlist cache.'))
+    .catch(err => console.log(`Could not update djlist cache ${err.message}`));
+};
 
 let cache = [];
-const getData = () => cache;
+updateCache(cache);
+setInterval(() => updateCache(cache), 60 * 1000 * 10);
+const getCache = () => cache;
 
-updateCache();
-setInterval(updateCache, 60 * 1000 * 10);
-
-module.exports = { getData };
+module.exports = { getCache };
